6cf228417a85d51ae9d6bb3c8f9e00c9440ddb38
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-10-04 19:50:22 +0200

    Refactor App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component, Fragment } from "react";
+import React, { useEffect, Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Grid } from "semantic-ui-react";
 import { handleInitialData } from "./actions/shared";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Login from "./components/Login";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
@@ -11,42 +11,43 @@ import NewPoll from "./components/NewPoll";
 import Leaderboard from "./components/Leaderboard";
 import NoMatch from "./components/NoMatch";
 
-class App extends Component {
-  componentDidMount() {
-    this.props.handleInitialData();
-  }
-  render() {
-    const { authUser } = this.props;
-    return (
-      <Router>
-        <div className="App">
-          {authUser === null ? (
-            <Route
-              render={() => (
-                <ContentGrid>
-                  <Login />
-                </ContentGrid>
-              )}
-            />
-          ) : (
-            <Fragment>
-              <Nav />
+export default function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(handleInitialData());
+  }, [dispatch]);
+
+  const authUser = useSelector((state) => state.authUser);
+
+  return (
+    <Router>
+      <div className="App">
+        {authUser === null ? (
+          <Route
+            render={() => (
               <ContentGrid>
-                <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route path="/questions/bad_id" component={NoMatch} />
-                  <Route path="/questions/:question_id" component={UserCard} />
-                  <Route path="/add" component={NewPoll} />
-                  <Route path="/leaderboard" component={Leaderboard} />
-                  <Route component={NoMatch} />
-                </Switch>
+                <Login />
               </ContentGrid>
-            </Fragment>
-          )}
-        </div>
-      </Router>
-    );
-  }
+            )}
+          />
+        ) : (
+          <Fragment>
+            <Nav />
+            <ContentGrid>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/questions/bad_id" component={NoMatch} />
+                <Route path="/questions/:question_id" component={UserCard} />
+                <Route path="/add" component={NewPoll} />
+                <Route path="/leaderboard" component={Leaderboard} />
+                <Route component={NoMatch} />
+              </Switch>
+            </ContentGrid>
+          </Fragment>
+        )}
+      </div>
+    </Router>
+  );
 }
 
 const ContentGrid = ({ children }) => (
@@ -56,11 +57,3 @@ const ContentGrid = ({ children }) => (
     </Grid.Row>
   </Grid>
 );
-
-function mapStateToProps({ authUser }) {
-  return {
-    authUser,
-  };
-}
-
-export default connect(mapStateToProps, { handleInitialData })(App);
